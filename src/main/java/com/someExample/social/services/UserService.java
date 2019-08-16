f20d0a951bcc069518c6a4c9b8f90c1dc4dc40d0
Kirill Tseitlin <Kirillphonelg@gmail.com>
2019-08-16 15:04:26 +0300

    jdbcAuthentication().dataSource(dataSource) replaced with UserDetailsService

---
+++ src/main/java/com/someExample/social/services/UserService.java
@@ -0,0 +1,23 @@
+package com.someExample.social.services;
+
+
+import com.someExample.social.reps.UserRepo;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepo userRepo;
+
+    public UserService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepo.findByUsername(username);
+    }
+}
